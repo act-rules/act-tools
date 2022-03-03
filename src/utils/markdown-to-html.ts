295c08df0a6793466b13bc0ec4cf6e9ebda2a47e
Wilco Fiers <wilco.fiers@deque.com>
2022-03-03 12:53:56 +0100

    fix(types): unified type issue

--- src/utils/markdown-to-html.ts
+++ src/utils/markdown-to-html.ts
@@ -5,9 +5,10 @@ import rehypeStringify from "rehype-stringify";
 
 export function markdownToHtml(markdown: string): string {
   const processor = unified()
-    .use(remarkParse)
-    .use(remarkRehype)
-    .use(rehypeStringify);
+    //  Use "as any" to avoid build problems; This is fine.
+    .use(remarkParse as any)
+    .use(remarkRehype as any)
+    .use(rehypeStringify as any);
 
   return String(processor.processSync(markdown));
 }
