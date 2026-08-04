7cfd5912877d9b6e7b76b7155ada10aea2840627
Daniel Montalvo <49305434+daniel-montalvo@users.noreply.github.com>
2026-08-04 13:47:31 +0200

    Update ACT Rules Format links to point to TR (#58)
    * Update ACT Rules Format links to point to TR
    
    * Restore accidental prettier changes

--- src/rule-transform/rule-content/get-requirements-map.ts
+++ src/rule-transform/rule-content/get-requirements-map.ts
@@ -12,7 +12,7 @@ export const headingText = "Accessibility Requirements Mapping";
 export const secondaryReqText = outdent`
   This rule is related to the following accessibility requirements, but was 
   not designed to test this requirements directly. These 
-  [secondary requirements](https://w3c.github.io/wcag-act/act-rules-format.html#secondary-requirements)
+  [secondary requirements](https://www.w3.org/TR/act-rules-format/#secondary-requirements)
   can either be stricter than the rule requires, or may be satisfied in ways 
   not tested by the rule:
 `;
